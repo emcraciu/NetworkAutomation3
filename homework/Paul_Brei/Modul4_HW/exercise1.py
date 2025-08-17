235256e4690d3c7d5f0aac2c432c3b5a940f99fc
breixs <paulbrei25@gmail.com>
2025-08-17 18:55:18 +0300

    Homework Paul Brei Module 4

---
+++ homework/Paul_Brei/Modul4_HW/exercise1.py
@@ -0,0 +1,24 @@
+import json
+import yaml
+
+def get_switch_info():
+    info={}
+    n = int(input("Enter the number of entries: "))
+    info = {input("Enter key: "): input("Enter value: ") for _ in range(n)}
+
+    return info
+
+def dict_to_json(dictionary):
+    return json.dumps(dictionary)
+
+def dict_to_yaml(dictionary):
+    return yaml.dump(dictionary)
+
+sw_info=get_switch_info()
+
+json_string=dict_to_json(sw_info)
+yaml_string=dict_to_yaml(sw_info)
+
+print(f"json string:{json_string}")
+print(80*'-')
+print(f"yaml string:{yaml_string}")
